927b7c943ea6ce96eed9cdfbdbe4c896ed7dd6ae
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-08-19 18:33:36 -0500

    Actividad Didactica

--- src/config/global.js
+++ src/config/global.js
@@ -57,12 +57,12 @@ export default {
       },
     ],
     subMenu: [
-      /*{
+      {
         nombreRuta: 'actividad',
         icono: 'far fa-question-circle',
         titulo: 'Actividad didáctica',
         desarrolloContenidos: true,
-      },*/
+      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
